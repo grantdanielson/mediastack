3049b8e9e78fd529a79eccdc96a8c1d204f2e6d3
Grant Danielson <grantdanielson@aol.com>
2025-09-10 11:44:28 -0400

    delete unused files and content

--- get-apikeys.sh
+++ get-apikeys.sh
@@ -17,12 +17,12 @@ echo
 
 echo "Bazarr   API Key: " `yq -r '.auth.apikey' $FOLDER_FOR_DATA/bazarr/config/config.yaml` "  located in $FOLDER_FOR_DATA/bazarr/config/config.yaml"
 echo "Lidarr   API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/lidarr/config.xml` "  located in $FOLDER_FOR_DATA/lidarr/config.xml"
-echo "Mylar    API Key: " `grep '^api_key' $FOLDER_FOR_DATA/mylar/mylar/config.ini | sed -E 's/.*=\s*//'` "  located in $FOLDER_FOR_DATA/mylar/mylar/config.ini"
+# echo "Mylar    API Key: " `grep '^api_key' $FOLDER_FOR_DATA/mylar/mylar/config.ini | sed -E 's/.*=\s*//'` "  located in $FOLDER_FOR_DATA/mylar/mylar/config.ini"
 echo "Prowlarr API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/prowlarr/config.xml` "  located in $FOLDER_FOR_DATA/prowlarr/config.xml"
 echo "Radarr   API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/radarr/config.xml` "  located in $FOLDER_FOR_DATA/radarr/config.xml"
-echo "Readarr  API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/readarr/config.xml` "  located in $FOLDER_FOR_DATA/readarr/config.xml"
+# echo "Readarr  API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/readarr/config.xml` "  located in $FOLDER_FOR_DATA/readarr/config.xml"
 echo "Sonarr   API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/sonarr/config.xml` "  located in $FOLDER_FOR_DATA/sonarr/config.xml"
-echo "Whisparr API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/whisparr/config.xml` "  located in $FOLDER_FOR_DATA/whisparr/config.xml"
+# echo "Whisparr API Key: " `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/whisparr/config.xml` "  located in $FOLDER_FOR_DATA/whisparr/config.xml"
 
 
 # echo "Sonarr   API Key:" `xmllint --xpath "string(//Config/ApiKey)" $FOLDER_FOR_DATA/sonarr/config.xml`
